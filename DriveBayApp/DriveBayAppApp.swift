dcfd803a39e213e641b7526b0ecb69548a85ad40
Dev Patel <devp1400@gmail.com>
2025-11-22 12:25:02 -0500

    Initial Commit

---
+++ DriveBayApp/DriveBayAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  DriveBayAppApp.swift
+//  DriveBayApp
+//
+//  Created by Dev Patel on 2025-11-22.
+//
+
+import SwiftUI
+
+@main
+struct DriveBayAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
