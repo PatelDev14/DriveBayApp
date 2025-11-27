7a517e5de6e7cb8a86ce353fc76e8963fef9bb9e
Dev Patel <devp1400@gmail.com>
2025-11-26 19:14:37 -0500

    Revert "Login Page and Chatview Page working with code"
    This reverts commit 9e3e5041f2f0d23eaa87a76b89d35902da54f19b.

--- DriveBayApp/ContentView.swift
+++ DriveBayApp/ContentView.swift
@@ -1,16 +1,24 @@
+//
+//  ContentView.swift
+//  DriveBayApp
+//
+//  Created by Dev Patel on 2025-11-22.
+//
+
 import SwiftUI
 
 struct ContentView: View {
-    @EnvironmentObject var authService: AuthService
-    
     var body: some View {
         VStack {
-            Text("Welcome to DriveBay!")
-                .font(.largeTitle)
-            Button("Sign Out") {
-                try? authService.signOut()
-            }
-            .padding()
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
         }
+        .padding()
     }
+}
+
+#Preview {
+    ContentView()
 }
